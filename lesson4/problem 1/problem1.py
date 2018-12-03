fdc1faf8d41b5f52c37ee3f293da5dd7763bcf35
valentinasil <43757543+valentinasil@users.noreply.github.com>
2018-12-03 12:42:25 -0500

    Add files via upload

---
+++ lesson4/problem 1/problem1.py
@@ -0,0 +1,4 @@
+score = 2.9
+new_score = (score / 4) * 100
+new_score =round (new_score)
+print(new_score)
---
+++ lesson4/problem 3/problem3.py
@@ -0,0 +1,7 @@
+english = 89
+math = 95
+history = 87
+science = 84
+
+avarage = (english + math + history + science ) / 4 
+print(avarage)
